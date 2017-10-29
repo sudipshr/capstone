34a97938cc478bf898ca3d4cd9c49191288cd4f8
sudipshr <sudips@gwu.edu>
2017-10-29 18:09:26 -0400

    initial commit

--- app/src/main/java/localeconnect/me/localeconnect/LoginActivity.java
+++ app/src/main/java/localeconnect/me/localeconnect/LoginActivity.java
@@ -3,35 +3,38 @@ package localeconnect.me.localeconnect;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
-import android.content.pm.PackageManager;
-import android.support.annotation.NonNull;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
 import android.app.LoaderManager.LoaderCallbacks;
-
 import android.content.CursorLoader;
+import android.content.Intent;
 import android.content.Loader;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.AsyncTask;
-
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
-import android.view.KeyEvent;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.inputmethod.EditorInfo;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
-import android.widget.TextView;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.MobileAds;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -68,10 +71,15 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
     private AdView mAdView;
 
+    private CheckBox mCheckbox;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mCheckbox = (CheckBox) findViewById(R.id.checkBox);
         // Set up the login form.
         mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
         populateAutoComplete();
@@ -90,11 +98,12 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
                 .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
                 .build();
 
+
         // Start loading the ad in the background.
         mAdView.loadAd(adRequest);
 
         mPasswordView = (EditText) findViewById(R.id.password);
-        mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+/*        mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
                 if (id == R.id.login || id == EditorInfo.IME_NULL) {
@@ -103,13 +112,21 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
                 }
                 return false;
             }
-        });
+        });*/
 
-        Button mEmailSignInButton = (Button) findViewById(R.id.email_sign_in_button);
-        mEmailSignInButton.setOnClickListener(new OnClickListener() {
+        Button mSignInButton = (Button) findViewById(R.id.sign_in_button);
+        mSignInButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
-                attemptLogin();
+                attemptLogin(false);
+            }
+        });
+
+        Button mRegisterButton = (Button) findViewById(R.id.register_button);
+        mRegisterButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                attemptLogin(true);
             }
         });
 
@@ -166,7 +183,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
      * If there are form errors (invalid email, missing fields, etc.), the
      * errors are presented and no actual login attempt is made.
      */
-    private void attemptLogin() {
+    private void attemptLogin(boolean isRegister) {
         if (mAuthTask != null) {
             return;
         }
@@ -178,6 +195,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         // Store values at the time of the login attempt.
         String email = mEmailView.getText().toString();
         String password = mPasswordView.getText().toString();
+        String userName = ((EditText)findViewById(R.id.userName)).getText().toString();
 
         boolean cancel = false;
         View focusView = null;
@@ -189,15 +207,23 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             cancel = true;
         }
 
+        if (!TextUtils.isEmpty(userName) && !isPasswordValid(userName)) {
+            mPasswordView.setError(getString(R.string.error_invalid_password));
+            focusView = mPasswordView;
+            cancel = true;
+        }
+
         // Check for a valid email address.
-        if (TextUtils.isEmpty(email)) {
-            mEmailView.setError(getString(R.string.error_field_required));
-            focusView = mEmailView;
-            cancel = true;
-        } else if (!isEmailValid(email)) {
-            mEmailView.setError(getString(R.string.error_invalid_email));
-            focusView = mEmailView;
-            cancel = true;
+        if (isRegister){
+            if (TextUtils.isEmpty(email)) {
+                mEmailView.setError(getString(R.string.error_field_required));
+                focusView = mEmailView;
+                cancel = true;
+            } else if (!isEmailValid(email)) {
+                mEmailView.setError(getString(R.string.error_invalid_email));
+                focusView = mEmailView;
+                cancel = true;
+            }
         }
 
         if (cancel) {
@@ -208,9 +234,11 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             // Show a progress spinner, and kick off a background task to
             // perform the user login attempt.
             showProgress(true);
-            mAuthTask = new UserLoginTask(email, password);
+            mAuthTask = new UserLoginTask(email, password, userName, false);
             mAuthTask.execute((Void) null);
         }
+
+        navigateToHomeScreen(mLoginFormView);
     }
     private boolean isEmailValid(String email) {
         //TODO: Replace this with your own logic
@@ -320,10 +348,14 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
         private final String mEmail;
         private final String mPassword;
+        private final String mUserName;
+        private boolean mStubMode;
 
-        UserLoginTask(String email, String password) {
+        UserLoginTask(String email, String password, String userName, boolean stubMode) {
             mEmail = email;
             mPassword = password;
+            mUserName = userName;
+            mStubMode = stubMode;
         }
 
         @Override
@@ -337,13 +369,37 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
                 return false;
             }
 
-            for (String credential : DUMMY_CREDENTIALS) {
-                String[] pieces = credential.split(":");
-                if (pieces[0].equals(mEmail)) {
-                    // Account exists, return true if the password matches.
-                    return pieces[1].equals(mPassword);
+            if (!mStubMode){
+                try {
+                    String url = "http://rest-service.guides.spring.io/greeting";
+                    url = "http://localhost:9000/eventserv";
+
+                    User user = new User();
+                    user.setDeviceId("sdfdsdfsfd");
+                    user.setUserName(String.valueOf(System.currentTimeMillis()));
+                    RestTemplate restTemplate = new RestTemplate();
+                    HttpEntity<User> request = new HttpEntity<>(user);
+                    ResponseEntity<User> entity = restTemplate.exchange(url, HttpMethod.POST,
+                            request, User.class);
+
+                    //restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
+                    //User user = restTemplate.getForObject(url, User.class);
+                    //restTemplate.exchange()
+                    Log.i("return msg:",user.toString());
+                } catch (Exception e) {
+                    Log.e("MainActivity", e.getMessage(), e);
                 }
             }
+            else {
+                for (String credential : DUMMY_CREDENTIALS) {
+                    String[] pieces = credential.split(":");
+                    if (pieces[0].equals(mEmail)) {
+                        // Account exists, return true if the password matches.
+                        return pieces[1].equals(mPassword);
+                    }
+                }
+            }
+
 
             // TODO: register the new account here.
             return true;
@@ -367,6 +423,13 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             mAuthTask = null;
             showProgress(false);
         }
+    }
+
+    public void navigateToHomeScreen(View view) {
+        Intent intent = new Intent(this, HomeScreen.class);
+
+        intent.putExtra("test", "Home Screen.....Under Construction");
+        startActivity(intent);
     }
 }
 
--- app/src/test/java/localeconnect/me/localeconnect/ExampleUnitTest.java
+++ app/src/test/java/localeconnect/me/localeconnect/ExampleUnitTest.java
@@ -1,17 +1,45 @@
 package localeconnect.me.localeconnect;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
 
-import static org.junit.Assert.*;
+import java.util.Date;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
+@RunWith(RobolectricTestRunner.class)
+@Config(manifest= Config.NONE)
 public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
+
+
+        String url = "http://rest-service.guides.spring.io/greeting";
+        url = "http://localhost:9000/eventserv/";
+
+        Event user = new Event();
+        user.setAcceptingUserId(
+                "sdfdsdfsfd");
+        user.setEventTime(new Date());
+        user.setAddress(String.valueOf(System.currentTimeMillis()));
+
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
+        HttpEntity<Event> request = new HttpEntity<>(user);
+        ResponseEntity<Event> entity = restTemplate.exchange(url, HttpMethod.POST,
+                request, Event.class);
     }
 }
